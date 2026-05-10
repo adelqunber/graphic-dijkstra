5c7ea6054a2a010def9557f001ebd931bbcdb3be
aj-1738 <ahmadjam563@gmail.com>
2026-05-10 16:13:11 +0300

    Header for GUI.c

--- GUI.h
+++ GUI.h
@@ -3,6 +3,8 @@
 
 #include "graph.h"
 
+// Header for Milestone2 and Milestone3
+
 void show_graph_static(const Graph* graph);
 void show_graph_animation(const Graph* graph, const int* path, int path_length);
 
